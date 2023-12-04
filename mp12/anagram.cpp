687f3a5ccf15df26b524fec590259f54e9298e52
matia6170 <hyunwoo6@illinois.edu>
2023-12-03 21:55:00 -0600

    finished mp12

--- mp12/anagram.cpp
+++ mp12/anagram.cpp
@@ -1,18 +1,25 @@
 #include "anagram.h"
 #include <iostream>
 #include <fstream>
+#include <string>
+/*
+MP12
+partners: hyunwoo6, crjung2, awinick2
+*/
 
 /***
 An AnagramDict constructor. This Constructor takes in a filepath
-and then creates a binary tree out of all the words inside the 
-binary tree. 
+and then creates a binary tree out of all the words inside the
+binary tree.
 
-Input: 
+Input:
 std::string filename - the path to the file
 
 Output:
 N/A
 ***/
+using namespace std;
+
 AnagramDict::AnagramDict(std::string filename) {
     filename_ = filename;
     std::string path = "dictionaries/" + filename_;
@@ -21,14 +28,19 @@ AnagramDict::AnagramDict(std::string filename) {
     /**
         Your code goes below here
     **/
+    string word;
+    while (file >> word) {
+        addWord(word);
+    }
+    file.close();
 
 }
 
 /***
-An AnagramDict member function. This function sorts the given word in 
+An AnagramDict member function. This function sorts the given word in
 lexicographical order
 
-Input: 
+Input:
 std::string word - the word that needs to be sorted
 
 Output:
@@ -38,7 +50,8 @@ std::string AnagramDict::sortWord(std::string word) {
     /**
         Your code goes below here
     **/
-   return std::string();
+    sort(word.begin(), word.end());
+    return word;
 }
 
 /***
@@ -47,7 +60,7 @@ If the words sorted version already exists in the tree add it to the linked
 list of that node. If the sorted word does not exist in the tree create a new
 node and insert it into the tree
 Duplicated words should not be added to the tree.
-Input: 
+Input:
 std::string word - the word that needs to inserted
 
 Output:
@@ -57,30 +70,80 @@ void AnagramDict::addWord(std::string word) {
     /**
         Your code goes below here
     **/
+    string sortedWord = sortWord(word);
+    list<string> wordlist;
+    Node<string, list<string> > *node = tree.find_node(sortedWord);
+
+    if (node == NULL) {
+        wordlist.push_back(word);
+        Node<string, list<string> > *newNode = new Node<string, list<string> >(sortedWord, wordlist);
+        tree.insert_node(tree.getRoot(), newNode);
+    } else {
+        
+        wordlist = node->getData();
+
+        list<string>::iterator it = wordlist.begin();
+        while (it != wordlist.end()) {
+            if (*it == word) {
+                return;
+            }
+            it++;
+        }
+        
+        wordlist.push_back(word);
+        node->setData(wordlist);
+    }
+
 }
 
 /***
 An AnagramDict member function. Does a preorder, postorder, or inorder traversal
 and then prints out all the anagrams and words.
 
-The output file should be the traversal order of the tree, but only the data on each line. 
-View on wiki for more information. 
+The output file should be the traversal order of the tree, but only the data on each line.
+View on wiki for more information.
 
-Input: 
+Input:
 std::string order - The type of order you want to traverse. Can be "pre", "post", "in"
 
 Output:
 N/A
 ***/
 void AnagramDict::saveAnagrams(std::string order) {
-    std::string path = "output/" + order+"_"+ filename_;
+    std::string path = "output/" + order + "_" + filename_;
     std::ofstream file;
     file.open(path.c_str());
-    if(!file.is_open()) {
+    if (!file.is_open()) {
         //create new file
-        file.open(path.c_str(),std::ios::out);
+        file.open(path.c_str(), std::ios::out);
     }
     /**
         Your code goes below here
     **/
+
+    list<Node<string, list<string> > > words;
+
+    if (order == "pre") {
+        tree.pre_order(tree.getRoot(), words);
+    } else if (order == "post") {
+        tree.post_order(tree.getRoot(), words);
+    } else if (order == "in") {
+        tree.in_order(tree.getRoot(), words);
+    }
+    list<Node<string, list<string> > >::iterator it = words.begin();
+    while(it != words.end()) {
+        list<string> wordlist = it->getData();
+
+        list<string>::iterator it2 = wordlist.begin();
+        while (it2 != wordlist.end()) {
+            file << *it2 << " ";
+            it2++;
+        }
+        file << endl;
+        it++;
+    }
+    file.close();
+
+
+
 }
--- mp12/binary.cpp
+++ mp12/binary.cpp
@@ -1,121 +1,127 @@
 #include "binary.h"
 using namespace std;
 
+/*
+MP12
+partners: hyunwoo6, crjung2, awinick2
+*/
+
+
 /***
 A Tree Node constructor. Set all the
 member variables (key_, data_, left, right)
-of the Node class. 
+of the Node class.
 
-Input: 
+Input:
 T key - The key for the node
 Y data - The actual value from the node
 
 Output:
 N/A
 ***/
-template <class T, class Y>
-Node<T, Y>::Node(T key, Y data){
-    /**
-        Your code goes below here
-    **/
-} 
+template <class T, class Y> Node<T, Y>::Node(T key, Y data) {
+  /**
+      Your code goes below here
+  **/
+  key_ = key;
+  data_ = data;
+  left = NULL;
+
+  right = NULL;
+}
 
 /***
 A Node member getter function. This returns the
-key of the node. 
+key of the node.
 
-Input: 
+Input:
 N/A
 
 Output:
 T - returns the key of the node that is of type T
 ***/
-template <class T, class Y>
-T Node<T, Y>::getKey(){
-    /**
-        Your code goes below here
-    **/
-   return (T) NULL;
+template <class T, class Y> T Node<T, Y>::getKey() {
+  /**
+      Your code goes below here
+  **/
+  return (T)key_;
 }
 
 /***
 A Node member getter function. This returns the
-actual data of the node. 
+actual data of the node.
 
-Input: 
+Input:
 N/A
 
 Output:
 Y - Returns the data of the node that is of type Y
 ***/
-template <class T, class Y>
-Y Node<T, Y>::getData(){
-    /**
-        Your code goes below here
-    **/
-   return (Y) 0;
+template <class T, class Y> Y Node<T, Y>::getData() {
+  /**
+      Your code goes below here
+  **/
+  return (Y)data_;
 }
 
 /***
 A Node member setter function. This sets the key
 of a Node.
 
-Input: 
+Input:
 T key - Sets the key of the Node of type T
 
 Output:
 N/A
 ***/
-template <class T, class Y>
-void Node<T, Y>::setKey(T key){
-    /**
-        Your code goes below here
-    **/
+template <class T, class Y> void Node<T, Y>::setKey(T key) {
+  /**
+      Your code goes below here
+  **/
+  key_ = key;
 }
 
 /***
 A Node member setter function. This sets the actual
 data of a Node.
 
-Input: 
+Input:
 Y data - Sets the value of the Node of type Y
 
 Output:
 N/A
 ***/
-template <class T, class Y>
-void Node<T, Y>::setData(Y data){
-    /**
-        Your code goes below here
-    **/
+template <class T, class Y> void Node<T, Y>::setData(Y data) {
+  /**
+      Your code goes below here
+  **/
+  data_ = data;
 }
 
-
 /***
-The BinaryTree constructor. Implicitly set the 
+The BinaryTree constructor. Implicitly set the
 values of the member variables
 
 
-Input: 
+Input:
 N/A
 
 Output:
 N/A
 ***/
 
-template <class T, class Y>
-BinaryTree<T,Y>::BinaryTree(){
-    /**
-        Your code goes below here
-    **/
+template <class T, class Y> BinaryTree<T, Y>::BinaryTree() {
+  /**
+      Your code goes below here
+  **/
+  root = NULL;
 }
-
 
 /***
 A BinaryTree Member fuction. This function takes in two arguments
 and inserts a node. If the key already exists in the tree do nothing.
 
-Input: 
+Input:
 Node<T,Y> *parent_node - The current root node
 Node<T,Y> *data - The Node that you want to insert into the tree
 
@@ -123,10 +129,28 @@ Output:
 N/A
 ***/
 template <class T, class Y>
-void BinaryTree<T,Y>::insert_node(Node<T,Y> *parent_node, Node<T,Y> *data){
-    /**
-        Your code goes below here
-    **/
+void BinaryTree<T, Y>::insert_node(Node<T, Y> *parent_node, Node<T, Y> *data) {
+  /**
+      Your code goes below here
+  **/
+  if (data == NULL) {
+    return;
+  }
+  if (parent_node == NULL) {
+    root = data;
+    return;
+  }
+  if (data->getKey() < parent_node->getKey()) {
+    if (parent_node->left != NULL)
+      insert_node(parent_node->left, data);
+    else
+      parent_node->left = data;
+  } else {
+    if (parent_node->right != NULL)
+      insert_node(parent_node->right, data);
+    else
+      parent_node->right = data;
+  }
 }
 
 /***
@@ -134,18 +158,27 @@ A BinaryTree Member fuction. This function takes in one arguments
 and finds the parent of a node given a key.
 If the key does not exist in the tree return NULL.
 
-Input: 
+Input:
 T key - The key of the node you are searching for.
 
 Output:
 Node<T,Y> * - returns the parent of the node you are searching for
 ***/
 template <class T, class Y>
-Node<T,Y> * BinaryTree<T,Y>::find_node_parent(T key){
-    /**
-        Your code goes below here
-    **/
-   return NULL;
+Node<T, Y> *BinaryTree<T, Y>::find_node_parent(T key) {
+  /**
+      Your code goes below here
+  **/
+  if (root == NULL || (*root).getKey() == key) {
+    return NULL;
+  }
+  if (((root->left != NULL) && (*(root->left)).getKey() == key) ||
+      ((root->right != NULL) && (*(root->right)).getKey() == key))
+    return root;
+  if (key < (*root).getKey())
+    return find_node_parent(root->left, key);
+  else
+    return find_node_parent(root->right, key);
 }
 
 /***
@@ -153,44 +186,54 @@ A BinaryTree Member fuction. This function takes in one arguments
 and finds the node given a key.
 If the key does not exist in the tree return NULL.
 
-Input: 
+Input:
 T key - The key of the node you are searching for.
 
 Output:
 Node<T,Y> * - returns the node you are searching for
 ***/
-template <class T, class Y>
-Node<T,Y> * BinaryTree<T,Y>::find_node(T key){
-    /**
-        Your code goes below here
-    **/
-   return NULL;
-}
+template <class T, class Y> Node<T, Y> *BinaryTree<T, Y>::find_node(T key) {
+  /**
+      Your code goes below here
+  **/
 
+  if (root == NULL) {
+    return NULL;
+  }
+  if ((*root).getKey() == key) {
+    return root;
+  }
+  if ((*root).getKey() > key)
+    return find_node(root->left, key);
+  else
+    return find_node(root->right, key);
+}
 
 /***
 A BinaryTree Member fuction. This function deletes a node in
 the binary tree. If the key does not exist do not do anything.
 
-Input: 
+Input:
 T key - The key of the node you are trying to delete.
 
 Output:
 N/A
 ***/
-template <class T, class Y>
-void BinaryTree<T,Y>::delete_node(T key){
-    /**
-        Your code goes below here
-    **/
+template <class T, class Y> void BinaryTree<T, Y>::delete_node(T key) {
+  /**
+      Your code goes below here
+  **/
+  if (root == NULL) {
+    return;
+  }
+  delete_node(root, key);
 }
-
 
 /***
 A BinaryTree helper fuction. This function deletes a node in
 the binary tree. If the key does not exist in the tree return NULL.
 
-Input: 
+Input:
 T key - The key of the node you are trying to delete.
 Node<T,Y>*node - The root of the current subtree
 
@@ -198,19 +241,48 @@ Output:
 Node<T,Y> * - The new root since the root can be deleted.
 ***/
 template <class T, class Y>
-Node<T,Y> * BinaryTree<T,Y>::delete_node(Node<T,Y>* node, T key){
-    /**
-        Your code goes below here
-    **/
-   return NULL;
-}
+Node<T, Y> *BinaryTree<T, Y>::delete_node(Node<T, Y> *node, T key) {
+  /**
+      Your code goes below here
+  **/
+  if (node == NULL) {
+    return NULL;
+  } else if (key < (*node).getKey()) {
+    node->left = delete_node(node->left, key);
+  } else if (key > (*node).getKey()) {
+    node->right = delete_node(node->right, key);
+  } else {                                           // Found the node
+    if (node->left == NULL && node->right == NULL) { // Case 1: No Child
+      delete node;
+      return NULL;
+    } else if (node->left == NULL) { // Case 2: Has no left child
+      Node<T, Y> *temp = node->right;
+      delete node;
+      return temp;
+    } else if (node->right == NULL) { // Case 3: Has no right child
+      Node<T, Y> *temp = node->left;
+      delete node;
+      return temp;
+    } else { // Case 4: Has two children
+      Node<T, Y> *temp = node->right;
+      while (temp->left != NULL) {
+        temp = temp->left;
+      }
+      node->setKey(temp->getKey());
+      node->setData(temp->getData());
+      node->right = delete_node(node->right, temp->getKey());
+    }
+  }
 
+  return node;
+}
 
 /***
 A BinaryTree helper fuction. This function finds the parent node of
-a node with a given key for the binary tree. If the key does not exist in the tree return NULL.
+a node with a given key for the binary tree. If the key does not exist in the
+tree return NULL.
 
-Input: 
+Input:
 T key - The key of the node you are trying to find.
 Node<T,Y>*node - The root of the current subtree
 
@@ -218,19 +290,31 @@ Output:
 Node<T,Y> * - Returns the parent of the key you were searching for
 ***/
 template <class T, class Y>
-Node<T,Y> * BinaryTree<T,Y>::find_node_parent(Node<T,Y> *node,T key){
-    /**
-        Your code goes below here
-    **/
-   return NULL;
+Node<T, Y> *BinaryTree<T, Y>::find_node_parent(Node<T, Y> *node, T key) {
+  /**
+      Your code goes below here
+  **/
+
+  if (node == NULL) {
+    return NULL;
+  }
+  if (((node->left != NULL) && (*(node->left)).getKey() == key) ||
+      ((node->right != NULL) && (*(node->right)).getKey() == key))
+    return node;
+
+  if (key < (*node).getKey())
+    return find_node_parent(node->left, key);
+  else if (key > (*node).getKey())
+    return find_node_parent(node->right, key);
+  return NULL;
 }
 
 /***
-A BinaryTree helper fuction. This function finds the node with 
-a given key for the binary tree. If the key does not exist in 
+A BinaryTree helper fuction. This function finds the node with
+a given key for the binary tree. If the key does not exist in
 the tree return NULL.
 
-Input: 
+Input:
 T key - The key of the node you are trying to find.
 Node<T,Y>*node - The root of the current subtree
 
@@ -239,11 +323,21 @@ Node<T,Y> * - Returns the Node in the Tree
               for the key you were searching for
 ***/
 template <class T, class Y>
-Node<T,Y> * BinaryTree<T,Y>::find_node(Node<T,Y> *node,T key){
-    /**
-        Your code goes below here
-    **/
-   return NULL;
+Node<T, Y> *BinaryTree<T, Y>::find_node(Node<T, Y> *node, T key) {
+  /**
+      Your code goes below here
+  **/
+  if (node == NULL) {
+    return NULL;
+  }
+  if ((*node).getKey() == key) {
+    return node;
+  }
+
+  if (key < (*node).getKey())
+    return find_node(node->left, key);
+  else
+    return find_node(node->right, key);
 }
 
 /***
@@ -251,18 +345,25 @@ A BinaryTree member fuction. This function performs
 pre_order traversal on the tree and stores the nodes
 inside a list
 
-Input: 
+Input:
 Node<T,Y> *node - The current root of the subtree
-List<Node<T,Y>> - A list to store all the nodes.
+List<Node<T,Y> > - A list to store all the nodes.
 
 Output:
 N/A
 ***/
 template <class T, class Y>
-void BinaryTree<T,Y>::pre_order(Node<T,Y> *node, std::list<Node<T,Y> > &list){
-    /**
-        Your code goes below here
-    **/
+void BinaryTree<T, Y>::pre_order(Node<T, Y> *node,
+                                 std::list<Node<T, Y> > &list) {
+  /**
+      Your code goes below here
+  **/
+  if (node == NULL) {
+    return;
+  }
+  list.push_back(*node);
+  pre_order(node->left, list);
+  pre_order(node->right, list);
 }
 
 /***
@@ -270,18 +371,24 @@ A BinaryTree member fuction. This function performs
 in_order traversal on the tree and stores the nodes
 inside a list
 
-Input: 
+Input:
 Node<T,Y> *node - The current root of the subtree
-List<Node<T,Y>> - A list to store all the nodes.
+List<Node<T,Y> > - A list to store all the nodes
 
 Output:
 N/A
 ***/
 template <class T, class Y>
-void BinaryTree<T,Y>::in_order(Node<T,Y> *node, std::list<Node<T,Y> > &list){
-    /**
-        Your code goes below here
-    **/
+void BinaryTree<T, Y>::in_order(Node<T, Y> *node, std::list<Node<T, Y> > &list) {
+  /**
+      Your code goes below here
+  **/
+  if (node == NULL) {
+    return;
+  }
+  in_order(node->left, list);
+  list.push_back(*node);
+  in_order(node->right, list);
 }
 
 /***
@@ -289,54 +396,61 @@ A BinaryTree member fuction. This function performs
 post_order traversal on the tree and stores the nodes
 inside a list
 
-Input: 
+Input:
 Node<T,Y> *node - The current root of the subtree
-List<Node<T,Y>> - A list to store all the nodes.
+List<Node<T,Y> > - A list to store all the nodes.
 
 Output:
 N/A
 ***/
 template <class T, class Y>
-void BinaryTree<T,Y>::post_order(Node<T,Y> *node, std::list<Node<T,Y> > &list){
-    /**
-        Your code goes below here
-    **/
+void BinaryTree<T, Y>::post_order(Node<T, Y> *node,
+                                  std::list<Node<T, Y> > &list) {
+  /**
+      Your code goes below here
+  **/
+  if (node == NULL) {
+    return;
+  }
+  post_order(node->left, list);
+  post_order(node->right, list);
+  list.push_back(*node);
 }
 
 /***
 A BinaryTree getter fuction. This function gets
 the current root of the binary tree.
 
-Input: 
+Input:
 N/A
 Output:
 Outputs the root of the binary tree
 ***/
-template <class T, class Y>
-Node<T,Y> * BinaryTree<T,Y>::getRoot(){
-    /**
-        Your code goes below here
-    **/
-   return NULL;
+template <class T, class Y> Node<T, Y> *BinaryTree<T, Y>::getRoot() {
+  /**
+      Your code goes below here
+  **/
+  return root;
 }
-
 
 /***
 The BinaryTree destructor. Make sure all
 the TreeNodes that have been created get destroyed
 and make sure there are no memory leaks.
 
-Input: 
+Input:
 N/A
 
 Output:
 N/A
 ***/
-template <class T, class Y>
-BinaryTree<T,Y>::~BinaryTree(){
-    /**
-        Your code goes below here
-    **/
+template <class T, class Y> BinaryTree<T, Y>::~BinaryTree() {
+  /**
+      Your code goes below here
+  **/
+
+  std::list<Node<T, Y> > list;
+  while (root != NULL && (root = delete_node(root, (*root).getKey())) != NULL);
 }
 
 template class Node<int, std::string>;
@@ -345,7 +459,7 @@ template class Node<int, int>;
 template class Node<double, double>;
 template class Node<std::string, long>;
 template class Node<std::string, std::list<std::string> >;
-template class Node<std::string, int >;
+template class Node<std::string, int>;
 
 template class BinaryTree<std::string, std::list<std::string> >;
 template class BinaryTree<std::string, std::string>;
